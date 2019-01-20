6d84f2affe9f1f60e12860be975f62a09ad241ed
Sandip Shrestha <san.inmersion@gmail.com>
2019-01-21 01:07:10 +0545

    Cleaning up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PokeDex/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokeDex from './components/PokeDex.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PokeDex
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/PokeDex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pokedex">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokeDex',
+  data() {
+    return {
+      title: 'Pokedex',
+    }
+  }
+}
+</script>
+
